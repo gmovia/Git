6db8b47a7599674d4789afc301b1f71a87001d51
gmovia <gmovia@fi.uba.ar>
2023-10-02 14:06:57 -0300

    add sets functions

---
+++ src/utils/sets/sets.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+// Recibe dos conjuntos.
+// Devuelve la diferencia entre ambos conjuntos. Lo que esta en A pero no esta en B.
+
+pub fn difference(a: HashMap<String, String>, b: HashMap<String, String>) -> HashMap<String, String>{
+    let mut hashmap = HashMap::new();
+    for (key, value) in a{
+        if !b.contains_key(&key){
+            hashmap.insert(key, value);
+        }
+    }
+    hashmap
+}
+
+// Recibe dos conjuntos.
+// Devuelve aquellos elementos que se encuentran en ambos conjuntos pero que su contenido es diferente.
+
+pub fn idem_set_different_content(a: HashMap<String, String>, b: HashMap<String, String>) -> HashMap<String, String>{
+    let mut hashmap = HashMap::new();
+    for (key, value) in a{
+        if let Some(value_in_b) = b.get(&key){
+            if value != *value_in_b{
+                hashmap.insert(key, value);
+            }
+         }
+    }
+    hashmap
+}
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,4 +2,8 @@ pub mod utils{
     pub mod files{
         pub mod files;
     }
+
+    pub mod sets{
+        pub mod sets;
+    }
 }
