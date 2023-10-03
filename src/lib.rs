fe55bb4ebd79064896d795629ed2ac777aa09c4a
gmovia <gmovia@fi.uba.ar>
2023-10-02 22:33:04 -0300

    add cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod utils {
         pub mod files;
     }
 
-    pub mod sets{
+    pub mod sets {
         pub mod sets;
     }
 }
+
+pub mod staging_area;
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,11 @@
-pub mod utils{
-    pub mod files{
+pub mod utils {
+    pub mod files {
         pub mod files;
     }
 
-    pub mod sets{
+    pub mod sets {
         pub mod sets;
     }
 }
+
+pub mod staging_area;
