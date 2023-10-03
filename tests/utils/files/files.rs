3380ff2b43b061c0e1a3e576e47fb842ce4b12bd
Luz M Diaz <ldiazc@fi.uba.ar>
2023-10-03 16:24:51 -0300

    merge with 2 branchs: status and staging_area

--- tests/utils/files/files.rs
+++ tests/utils/files/files.rs
@@ -3,121 +3,73 @@ mod tests {
     use rust_git::utils::files::files::read;
     use std::path::Path;
 
-    const TESTS_PATH: &str = "tests/utils/files";
-    const FOLDER_PATH: &str = "tests/utils/files/folder";
-    const FILE_1_PATH: &str = "tests/utils/files/file1.txt";
-    const FILE_2_PATH: &str = "tests/utils/files/file2.txt";
-    const FILE_3_PATH: &str = "tests/utils/files/folder/file3.txt";
-    const INEXIST_FILE_PATH: &str = "tests/utils/files/folder/file4.txt";
-    const TEST_ERROR: &str = "ERROR: An error ocurred while running the test";
-
     #[test]
     fn test_01_contain_file_1() {
-        let directory = Path::new(TESTS_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_1_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files");
+        let path = directory.join("file1.txt");
+        assert!(matches!(read(directory), Ok(files) if files.contains_key(&path.display().to_string())));
     }
 
     #[test]
     fn test_02_contain_file_2() {
-        let directory = Path::new(TESTS_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_2_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files");
+        let path = directory.join("file2.txt");
+        assert!(matches!(read(directory), Ok(files) if files.contains_key(&path.display().to_string())));
     }
 
     #[test]
     fn test_03_contain_file_3() {
-        let directory = Path::new(TESTS_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_3_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files");
+        let path = directory.join("folder").join("file3.txt");
+        assert!(matches!(read(directory), Ok(files) if files.contains_key(&path.display().to_string())));
     }
 
     #[test]
     fn test_04_contain_folder() {
-        let directory = Path::new(TESTS_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(FOLDER_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files");
+        let path = directory.join("folder");
+        assert!(matches!(read(directory), Ok(files) if files.contains_key(&path.display().to_string())));
     }
 
     #[test]
     fn test_05_contain_file_1() {
-        let file = Path::new(FILE_1_PATH);
-
-        match read(file) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_1_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let file = Path::new("tests/utils/files/file1.txt");
+        assert!(matches!(read(file), Ok(files) if files.contains_key(&file.display().to_string())));
     }
 
     #[test]
     fn test_06_not_contain_file_2() {
-        let directory = Path::new(FOLDER_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_2_PATH), false),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files/folder");
+        assert!(!matches!(read(directory), Ok(files) if files.contains_key("tests/utils/files/file2.txt")));
     }
 
     #[test]
     fn test_07_not_contain_file_4() {
-        let directory = Path::new(FOLDER_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.contains_key(INEXIST_FILE_PATH), false),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files/folder");
+        assert!(!matches!(read(directory), Ok(files) if files.contains_key("tests/utils/files/folder/file4.txt")));
     }
 
     #[test]
     fn test_08_contain_two_files() {
-        let directory = Path::new(FOLDER_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.len(), 2),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files/folder");
+        assert!(matches!(read(directory), Ok(files) if files.len() == 2));
     }
 
     #[test]
     fn test_09_contain_four_files() {
-        let directory = Path::new(TESTS_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.len(), 6),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files");
+        assert!(matches!(read(directory), Ok(files) if files.len() == 6));
     }
 
     #[test]
     fn test_10_contain_one_file() {
-        let directory = Path::new(FILE_1_PATH);
-
-        match read(directory) {
-            Ok(files) => assert_eq!(files.len(), 1),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let directory = Path::new("tests/utils/files/file1.txt");
+        assert!(matches!(read(directory), Ok(files) if files.len() == 1));
     }
 
     #[test]
     fn test_11_contain_file_1() {
-        let file = Path::new(FILE_1_PATH);
-
-        match read(file) {
-            Ok(files) => assert_eq!(files.contains_key(FILE_1_PATH), true),
-            Err(_) => panic!("{}", TEST_ERROR),
-        }
+        let file = Path::new("tests/utils/files/file1.txt");
+        assert!(matches!(read(file), Ok(files) if files.contains_key(&file.display().to_string())));
     }
 }
