0ced59b348643a15e2482369e99cb8fc76d173b1
gmovia <gmovia@fi.uba.ar>
2023-10-05 11:46:17 -0300

    add case git add .

--- tests/version_control_system/add.rs
+++ tests/version_control_system/add.rs
@@ -3,7 +3,7 @@ mod tests {
     use crate::tests_functions::{create_file, set_up, equals};
 
     #[test]
-    pub fn test_01_add_file_1_to_staging_area() {
+    pub fn test_01_add_file_1_to_staging_area() { // git add file1.txt
         let (temp_dir, mut vsc) = set_up();
         let path = create_file(&temp_dir, "file1.txt");        
 
@@ -12,7 +12,7 @@ mod tests {
     }
 
     #[test]
-    pub fn test_02_add_file_1_and_file_2_to_staging_area() {
+    pub fn test_02_add_file_1_and_file_2_to_staging_area() { // git add file1.txt y despues git add file2.txt
         let (temp_dir, mut vsc) = set_up();
         let file1_path = create_file(&temp_dir, "file1.txt");   
         let file2_path = create_file(&temp_dir, "file2.txt");        
@@ -24,7 +24,7 @@ mod tests {
     }
 
     #[test]
-    pub fn test_03_add_file_1_the_status_is_created() {
+    pub fn test_03_add_file_1_the_status_is_created() { // git add file1.txt
         let (temp_dir, mut vsc) = set_up();
         let path = create_file(&temp_dir, "file1.txt");      
 
@@ -34,7 +34,7 @@ mod tests {
 
 
     #[test]
-    pub fn test_04_add_file_1_the_status_is_modified() {
+    pub fn test_04_add_file_1_the_status_is_modified() { // git add file1.txt
         let (temp_dir, mut vsc) = set_up();
         let path = create_file(&temp_dir, "file1.txt");      
 
@@ -46,7 +46,7 @@ mod tests {
 
 
     #[test]
-    pub fn test_05_if_file_1_is_in_staging_area_and_add_file_1_updated_staging_area() {
+    pub fn test_05_if_file_1_is_in_staging_area_and_add_file_1_updated_staging_area() { // git add file1.txt y despues git add file1.txt
         let (temp_dir, mut vsc) = set_up();
         let path = create_file(&temp_dir, "file1.txt");      
 
@@ -59,7 +59,7 @@ mod tests {
     }
 
     #[test]
-    pub fn test_06_if_file_1_is_in_staging_area_and_add_file_1_updated_staging_area() {
+    pub fn test_06_if_file_1_is_in_staging_area_and_add_file_1_updated_staging_area() { // git add file1.txt
         let (temp_dir, mut vsc) = set_up();
         let path = create_file(&temp_dir, "file1.txt");      
 
@@ -68,9 +68,15 @@ mod tests {
 
         assert!(matches!(staging_area, Ok(staging_area) if equals(staging_area.clone(), &path, "CREATED")));
     }
+
+    #[test]
+    pub fn test_07_add_all_files_to_staging_area() { // git add .
+        let (temp_dir, mut vsc) = set_up();
+        let _ = create_file(&temp_dir, "file1.txt");
+        let _ = create_file(&temp_dir, "file2.txt");        
+        let _ = create_file(&temp_dir, "file3.txt");     
+
+        let staging_area = vsc.add(std::path::Path::new(&vsc.path.clone()));
+        assert!(matches!(staging_area, Ok(staging_area) if staging_area.len() == 3))
+    }
 }
-
-
-    // Falta contemplar
-        // varios cambios => git add archivo1.txt archivo2.txt
-        // todos los cambios => git add .
