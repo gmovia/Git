30aec7ef7aafa916df333ab0705e531ecd940fa9
ldefeo <ldefeo@fi.uba.ar>
2023-10-12 19:20:39 -0300

    add cat_file tests

---
+++ tests/vcs/cat_file.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    use std::{path::Path, fs::{File, self}, io::{Write, Read}};
+
+    use rust_git::vcs::version_control_system::VersionControlSystem;
+    use rust_git::vcs::commands::hash_object::WriteOption;
+    use crate::tests_functions::{create_file, set_up};
+
+    #[test]
+    pub fn test_01_read_hash_of_file_1_with_cat_file() -> Result<(), std::io::Error> {
+
+        let (temp_dir, _) = set_up();
+        let path = create_file(&temp_dir, "file1.txt");  
+        
+        let hash = VersionControlSystem::hash_object(&path, WriteOption::Write)?;
+
+        assert_eq!(VersionControlSystem::cat_file(&hash)?,fs::read_to_string(path)?);
+        Ok(())
+    }
+    #[test]
+    pub fn test_02_tried_to_read_content_with_cat_file_of_an_unexist_hash_object() -> Result<(), std::io::Error> {
+
+        assert!(matches!(VersionControlSystem::cat_file("asdasd"), Err(e) if e.to_string().contains("No such file or directory")));    
+        Ok(())
+    }
+
+}
