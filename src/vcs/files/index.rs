f32a9d9639484a1f22894086661f093c22242b0e
gmovia <gmovia@fi.uba.ar>
2023-10-13 01:22:27 -0300

    add comments

--- src/vcs/files/index.rs
+++ src/vcs/files/index.rs
@@ -1,6 +1,8 @@
 use crate::vcs::files::vcs_file::VCSFile;
 use std::{path::Path, fs::{self, OpenOptions, File}, io::Write, collections::HashMap};
 
+/// Recibe el area de staging.
+/// Vacia index y luego escribe cada archivo del area de staging en el.
 pub fn write(staging_area: &HashMap<String, VCSFile>) -> Result<(),std::io::Error>{
     fs::create_dir_all(".rust_git")?;
     let index_path = Path::new(".rust_git/index");
@@ -12,12 +14,16 @@ pub fn write(staging_area: &HashMap<String, VCSFile>) -> Result<(),std::io::Erro
     Ok(())
 }
 
+/// Recibe index.
+/// Vacia index.
 pub fn clear(index: &mut File) -> Result<(), std::io::Error>{
     let _ = index.set_len(0);
     Ok(())
 }
 
-pub fn add(index: &mut File,file: &VCSFile) -> Result<(),std::io::Error> {
+/// Recibe index y un archivo
+/// Escribe el archivo en index
+pub fn add(index: &mut File, file: &VCSFile) -> Result<(),std::io::Error> {
     index.write_all(file.path.as_bytes())?;
     index.write_all("-".as_bytes())?;
     index.write_all(file.state.as_bytes())?;
