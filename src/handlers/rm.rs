5682762d4bb052a26bca2b21f397010afb90f658
Luz M. Diaz Calixto <ldiazc@fi.uba.ar>
2023-10-14 21:02:28 -0300

    add rm in main

--- src/handlers/rm.rs
+++ src/handlers/rm.rs
@@ -2,8 +2,8 @@ use std::path::Path;
 
 use crate::vcs::{version_control_system::VersionControlSystem, commands::rm::RemoveOption};
 
-/// Recibe input del comando
-/// Devuelve el 
+/// Recibe input del comando rm puede ser "rm path" o "rm -r path"
+/// Se setea la option correspondiente de acuerdo al comando 
 pub fn handler_rm(vcs: &mut VersionControlSystem, input: String) -> Result<(), std::io::Error>{
     let args: Vec<&str> = input.split_whitespace().collect();
     let vcs_path = String::from(vcs.path.clone());
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Write};
 
+use rust_git::handlers::rm::handler_rm;
 use rust_git::vcs::version_control_system::VersionControlSystem;
 use rust_git::handlers::{status::handler_status, add::handler_add, hash_object::handler_hash_object, cat_file::handler_cat_file};
 
@@ -8,6 +9,8 @@ fn main() -> Result<(), std::io::Error>{
     //let mut vcs = VersionControlSystem::init("/Users/gmovia/Desktop/PRUEBA", Vec::new());
     //let mut vcs = VersionControlSystem::init(r"C:\Users\Administrator\Desktop\PRUEBA\", Vec::new());
     let mut vcs = VersionControlSystem::init(r"C:\Users\laura\OneDrive\Escritorio\FIUBA\Taller de programacion I\Trabajo practico grupal\PRUEBA", Vec::new());
+    //let mut vcs = VersionControlSystem::init("/Users/luz.diazc/Desktop/PruebaTaller", Vec::new());
+
     loop{
         let mut input = String::new();
         io::stdout().flush().unwrap();
@@ -19,7 +22,8 @@ fn main() -> Result<(), std::io::Error>{
             "git status" => handler_status(&vcs),
             x if x.contains("git hash-object") => {println!("{:?}",handler_hash_object(&vcs, x.to_string())?);} ,
             x if x.contains("git add") => {handler_add(&mut vcs, x.to_string())?;},
-            x if x.contains("git cat-file") => {println!("{:?}",handler_cat_file(&vcs, x.to_string())?);}
+            x if x.contains("git cat-file") => {println!("{:?}",handler_cat_file(&vcs, x.to_string())?);},
+            x if x.contains("git rm") => {handler_rm(&mut vcs, x.to_string())?;},
             _ => {}
         }
     }
