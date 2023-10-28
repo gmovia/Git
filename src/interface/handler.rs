0d4b8ee2d431266513e3da63e8a18150c63ab209
ldefeo <ldefeo@fi.uba.ar>
2023-10-28 19:59:11 -0300

    refactor and changes in commit and branch handlers interface

--- src/interface/handler.rs
+++ src/interface/handler.rs
@@ -10,22 +10,37 @@ pub fn handle_commit(interface: &RustInterface, vcs: &VersionControlSystem){
     let dialog = interface.commit_dialog.clone();
     let version = Rc::new(RefCell::new(vcs.clone()));
     let rc_entry = Rc::new(RefCell::new(interface.message.clone()));
-    interface.commit_button.connect_clicked({
-        move |_| {        
-        dialog.run();
-        box_window.foreach(|child|{
-            box_window.remove(child);
-        });
-        dialog.hide();
+    let rc_ok = Rc::new(RefCell::new(interface.message_ok.clone()));
+    
+    interface.message_ok.set_sensitive(false);
+
+    interface.message.connect_changed({
+        let rc_ok = rc_ok.clone();
+        move |e| {
+        let rc_ok = rc_ok.borrow_mut();
+        rc_ok.set_sensitive(!e.text().is_empty());
     }});
 
+    interface.commit_button.connect_clicked({
+        move |_| {        
+            dialog.run();
+            dialog.hide();
+            box_window.foreach(|child| {
+                box_window.remove(child);
+            });
+        }
+    });
     interface.message_ok.connect_clicked({
         let rc_entry = rc_entry.clone();
         let version = version.clone();
-        move |_| {
+        move |button| {
             let mut version = version.borrow_mut();
             let rc_entry = rc_entry.borrow_mut();
+
             let _ = version.commit(rc_entry.text().to_string());
+
+            rc_entry.set_text("");
+            button.set_sensitive(false);
         }
     });
     
@@ -37,6 +52,16 @@ pub fn handle_branch(interface: &RustInterface, vcs: &VersionControlSystem) {
     let version = Rc::new(RefCell::new(vcs.clone()));
     let rc_entry = Rc::new(RefCell::new(interface.dialog_entry.clone()));
     let rc_branch = Rc::new(RefCell::new(interface.select_branch.clone()));
+    let rc_create = Rc::new(RefCell::new(interface.create_branch.clone()));
+
+    interface.create_branch.set_sensitive(false);
+
+    interface.dialog_entry.connect_changed({  
+        let rc_create = rc_create.clone();
+        move |e| {
+        let rc_create = rc_create.borrow_mut();
+        rc_create.set_sensitive(!e.text().is_empty());
+    }});
 
     interface.new_branch_button.connect_clicked(
         move |_| {
@@ -49,12 +74,17 @@ pub fn handle_branch(interface: &RustInterface, vcs: &VersionControlSystem) {
         let version = version.clone();
         let rc_entry = rc_entry.clone();
         let rc_branch = rc_branch.clone();
-        move |_|{
+        move |button|{
             let version = version.borrow_mut();
             let rc_entry = rc_entry.borrow_mut();
             let rc_branch = rc_branch.borrow_mut();
+
             let _ = version.branch(BranchOptions::NewBranch(&rc_entry.text()));
+
             rc_branch.append_text(&rc_entry.text());
+
+            rc_entry.set_text("");
+            button.set_sensitive(false);
     }});
 }
 
