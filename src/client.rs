aefced572d0db968847f674364f12b6c8edd50a6
AgustinMoralejo <amoralejo@gi.uba.ar>
2023-10-28 04:10:51 -0300

    refactor client

--- src/client.rs
+++ src/client.rs
@@ -8,49 +8,56 @@ static CLIENT_ARGS: usize = 4;
 //comando para levantar el git daemon --> git daemon --base-path=. --export-all --reuseaddr --informative-errors --verbose --verbose
 //comando para levantar cliente--> cargo run 127.0.0.1 9418 /TEST2
 
-//Checkear que este main te lo tome como main momentaneo
-pub fn client_(args: Vec<String>) -> Result<(), ()> {
-    if args.len() != CLIENT_ARGS {
-        println!("Cantidad de argumentos inválido");
-        println!("{:?} <host> <puerto>", &args[0]);
-        return Err(());
+pub struct Client;
+
+impl Client {
+
+    //Checkear que este main te lo tome como main momentaneo
+    pub fn client_(args: Vec<String>) -> Result<(), ()> {
+        if args.len() != CLIENT_ARGS {
+            println!("Cantidad de argumentos inválido");
+            println!("{:?} <host> <puerto>", &args[0]);
+            return Err(());
+        }
+        
+        let address = args[1].clone() + ":" + &args[2];
+        if let Err(e) = Self::client_run(&address, &args[3]) {
+            println!("Error: {}",e);
+        }
+        Ok(())
     }
-    
-    let address = args[1].clone() + ":" + &args[2];
-    if let Err(e) = client_run(&address, &args[3]) {
-        println!("Error: {}",e);
+
+
+    fn handler_query(query: &str,socket: &mut TcpStream ) {    
+        match query {
+            "git-upload-pack" => {
+                if let Err(e) = clone::Clone::clone(socket) {
+                    println!("Error: {}", e);
+                }            
+                println!("Handling git-upload-pack request");
+            }
+            "git-send-pack" => {
+                println!("Handling git-send-pack request");
+            }
+            _ => {
+                println!("Unknown request: {}", query);
+            }
+        }
     }
-    Ok(())
+
+    fn client_run(address: &str, path: &str) -> Result<(),std::io::Error> {
+
+        println!("Conectándome a {:?}", address);
+        let mut socket = TcpStream::connect(address)?;
+        let msg = format!("git-upload-pack {}", path);
+        let pkt_line = to_pkt_line(&msg);
+        socket.write(pkt_line.as_bytes())?;
+        
+        Self::handler_query("git-upload-pack",&mut socket );
+
+        Ok(())
+
+    }
+
 }
 
-
-fn handler_query(query: &str,socket: &mut TcpStream ) {    
-    match query {
-        "git-upload-pack" => {
-            if let Err(e) = clone::Clone::clone(socket) {
-                println!("Error: {}", e);
-            }            
-            println!("Handling git-upload-pack request");
-        }
-        "git-send-pack" => {
-            println!("Handling git-send-pack request");
-        }
-        _ => {
-            println!("Unknown request: {}", query);
-        }
-    }
-}
-
-fn client_run(address: &str, path: &str) -> Result<(),std::io::Error> {
-
-    println!("Conectándome a {:?}", address);
-    let mut socket = TcpStream::connect(address)?;
-    let msg = format!("git-upload-pack {}", path);
-    let pkt_line = to_pkt_line(&msg);
-    socket.write(pkt_line.as_bytes())?;
-    
-    handler_query("git-upload-pack",&mut socket );
-
-    Ok(())
-
-}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-// use std::io::{self, Write};
-// use std::path::Path;
+/* 
+use std::io::{self, Write};
+ use std::path::Path;
+ use rust_git::handlers::branch::handler_branch;
+ use rust_git::handlers::commit::handler_commit;
+ use rust_git::handlers::rm::handler_rm;
+ use rust_git::vcs::version_control_system::VersionControlSystem;
+ use rust_git::handlers::{status::handler_status, add::handler_add, hash_object::handler_hash_object, cat_file::handler_cat_file, log::handler_log};
+ */
+use rust_git::client::Client;
 
-// use rust_git::handlers::branch::handler_branch;
-// use rust_git::handlers::commit::handler_commit;
-// use rust_git::handlers::rm::handler_rm;
-// use rust_git::vcs::version_control_system::VersionControlSystem;
-// use rust_git::handlers::{status::handler_status, add::handler_add, hash_object::handler_hash_object, cat_file::handler_cat_file, log::handler_log};
-
-use rust_git::client::client_;
-
-
+ 
 fn main() -> Result<(), std::io::Error> {
     let argv = std::env::args().collect::<Vec<String>>();    
-    match client_(argv) {
+    match Client::client_(argv) {
         Ok(_) => println!("La función client se ejecutó correctamente"),
         Err(_) => println!("Hubo un error al ejecutar la función client....."),
     }
     Ok(())
 }
+/* 
+fn main() -> Result<(), std::io::Error> {
 
-   /*  let mut vcs = VersionControlSystem::init(Path::new("test_folder"), Vec::new());
+    let mut vcs = VersionControlSystem::init(Path::new("test_folder"), Vec::new());
     
     loop{
         let mut input = String::new();
@@ -32,11 +34,13 @@ fn main() -> Result<(), std::io::Error> {
             "git status" => handler_status(&vcs),
             x if x.contains("git hash-object") => {println!("{:?}",handler_hash_object(&vcs, x.to_string())?);} ,
             x if x.contains("git add") => {handler_add(&mut vcs, x.to_string())?;},
-            x if x.contains("git cat-file") => {println!("{:?}",handler_cat_file(&vcs, x.to_string())?);},
+            x if x.contains("git cat-file") => {println!("{:?}",handler_cat_file(&vcs, x.to_string(), ".rust_git")?);},
             x if x.contains("git rm") => {handler_rm(&mut vcs, x.to_string())?;},
             x if x.contains("git log") => {let _ = handler_log(&vcs);},
             x if x.contains("git commit") => {handler_commit(&mut vcs, x.to_string())?;},
             x if x.contains("git branch") => {handler_branch(&vcs, x.to_string())?;},
             _ => {}
         }
-    } */
+    } 
+}
+*/
