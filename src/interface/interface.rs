190989b7484789482350255f0e449e4acf2fcc38
AgustinMoralejo <amoralejo@gi.uba.ar>
2023-11-06 01:39:17 -0300

    fix fetch

--- src/interface/interface.rs
+++ src/interface/interface.rs
@@ -134,7 +134,7 @@ impl RustInterface {
     pub fn impl_interface(&self) -> Result<(), std::io::Error>{    
         set_styles_css(self);
         
-        VersionControlSystem::init(Path::new("clone_here"), Vec::new());
+        //VersionControlSystem::init(Path::new("clone_here"), Vec::new());
 
         let _ = changes_and_staging_area(&self.grid, &self.box_window);
         repositories( &self.select_repository)?;
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use std::{io::{self, Write}, path::Path};
 
 fn main() -> Result<(), std::io::Error>{
     //VersionControlSystem::init(Path::new("test_folder/repo_2"), Vec::new());
-    VersionControlSystem::init(Path::new("clone_here"), Vec::new());
+    //VersionControlSystem::init(Path::new("clone_here"), Vec::new());
+    /* 
      loop{
          let mut input = String::new();
          io::stdout().flush().unwrap();
          io::stdin().read_line(&mut input).unwrap();
          handler_command( &input);
      }
-    //let interface = RustInterface::new();
-    //let _ = interface.impl_interface();
+     */
+    let interface = RustInterface::new();
+    let _ = interface.impl_interface();
     Ok(())
     //
 }
