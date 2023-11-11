af9365e32408fde2ee6921e314a3c6793fb93a0b
gmovia <gmovia@fi.uba.ar>
2023-11-11 01:11:01 -0300

    add case TREE

--- src/vcs/entities/commit_entity.rs
+++ src/vcs/entities/commit_entity.rs
@@ -1,16 +1,15 @@
 use std::{path::{PathBuf, Path}, fs::{OpenOptions, self}, io::Write};
-use crate::vcs::commands::{hash_object::{HashObject, WriteOption}, init::Init, cat_file::CatFile};
+use crate::{vcs::commands::{hash_object::{HashObject, WriteOption}, init::Init, cat_file::CatFile}, utils::random::random::Random};
 
-pub struct CommitEntity{    // tree + el hash del tree
-    pub content_type: String, // "tree"
+pub struct CommitEntity{
+    pub content_type: String,
     pub tree_hash: String
 }
 
 impl CommitEntity{
-    /// Recibe el conjunto de blobs que se va a escribir y el path del repositorio
-    /// Crea el archivo de commit y devuelve su hash => el que se encuentra en la tabla de commits => id hash_commit message date
+
     pub fn write(repo_path: &PathBuf, tree_hash: &String) -> Result<String, std::io::Error>{
-        let commit_path = Path::new(&repo_path).join("commit");
+        let commit_path = Path::new(&repo_path).join(Random::random());
         let mut commit_file = OpenOptions::new().write(true).create(true).append(true).open(&commit_path)?; 
 
         let entry = format!("tree {}\n", tree_hash);
@@ -21,9 +20,7 @@ impl CommitEntity{
         let _ = fs::remove_file(commit_path);
         Ok(commit_hash)
     }
-
-    /// Recibe el hash del commit
-    /// Devuelve el contenido de ese commit => tree hash_tree
+    
     pub fn read(repo_path: &PathBuf, commit_hash: &str) -> Result<CommitEntity, std::io::Error>{
         let commit = CatFile::cat_file(commit_hash, Init::get_object_path(repo_path)?)?;
         let commit_lines: Vec<&str> = commit.split_whitespace().collect();
---
+++ src/vcs/entities/entity.rs
@@ -0,0 +1,65 @@
+use std::collections::{HashMap, HashSet};
+use super::{blob_entity::BlobEntity, tree_entity::TreeEntity};
+#[derive(Debug, Clone)]
+
+pub enum Entity{
+    Blob(BlobEntity),
+    Tree(TreeEntity),
+}
+
+pub fn convert_to_repository(entities: &Vec<Entity>) -> HashMap<String, String>{
+    let mut local_repository: HashMap<String, String> = HashMap::new();
+    for entity in entities{
+        match entity{
+            Entity::Blob(blob) => { local_repository.insert(blob.path.clone(), blob.blob_hash.clone()); }
+            Entity::Tree(tree) => { local_repository.extend(convert_to_repository(&tree.entities)); }
+        }
+    }
+    local_repository
+}
+
+pub fn convert_to_entities(hash_map: &HashMap<String, String>, prefix: &str) -> Vec<Entity> {
+    let mut entities = Vec::new();
+    let mut processed_paths = HashSet::new();
+
+    for (key, value) in hash_map.iter() {
+        if processed_paths.contains(key) {
+            continue;
+        }
+
+        if let Some(rest) = key.strip_prefix(prefix) {
+            let path_parts: Vec<&str> = rest.split('/').collect();
+
+            if path_parts.len() == 1 {
+                let entity = BlobEntity {
+                    content_type: "blob".to_string(),
+                    path: key.to_string(),
+                    blob_hash: value.to_string(),
+                };
+                entities.push(Entity::Blob(entity));
+                processed_paths.insert(key.clone());
+            } else {
+                let common_prefix = format!("{}{}/", prefix, path_parts[0]);
+                let subtree: HashMap<String, String> = hash_map
+                    .iter()
+                    .filter(|(k, _)| k.starts_with(&common_prefix))
+                    .map(|(k, v)| (k.clone(), v.clone()))
+                    .collect();
+
+                let tree_entities = convert_to_entities(&subtree, &common_prefix);
+
+                let tree = TreeEntity {
+                    content_type: "tree".to_string(),
+                    path: format!("{}{}", prefix, path_parts[0]),
+                    entities: tree_entities,
+                    tree_hash: "".to_string(),
+                };
+
+                entities.push(Entity::Tree(tree));
+                processed_paths.extend(subtree.keys().cloned());
+            }
+        }
+    }
+
+    entities
+}
