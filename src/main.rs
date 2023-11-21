9ee7671138fb4e0ee91eecbda48d746ddffd60ef
AgustinMoralejo <amoralejo@gi.uba.ar>
2023-11-21 18:10:15 -0300

    clone test

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use rust_git::{vcs::version_control_system::VersionControlSystem, handlers::comm
 use rust_git::interfaces::interface::RustInterface;
 
 fn main() -> Result<(), std::io::Error>{
-    // //let _ = VersionControlSystem::init(Path::new("test_folder/clone"), Vec::new());
-    // let _ = VersionControlSystem::init(Path::new("clone"), Vec::new());
+    let _ = VersionControlSystem::init(Path::new("tests/test_folder/clone_test"), Vec::new());
+    //let _ = VersionControlSystem::init(Path::new("clone"), Vec::new());
 
-    // loop{
-    //     let mut input = String::new();
-    //     io::stdout().flush().unwrap();
-    //     io::stdin().read_line(&mut input).unwrap();
-    //     handler_command( &input);
-    // }
-    let interface = RustInterface::new();
-    interface.impl_interface()?;
+    loop{
+         let mut input = String::new();
+         io::stdout().flush().unwrap();
+         io::stdin().read_line(&mut input).unwrap();
+         handler_command( &input);
+    }
+    //let interface = RustInterface::new();
+    //interface.impl_interface()?;
     Ok(())
 }
--- tests/vcs/clone.rs
+++ tests/vcs/clone.rs
@@ -1,14 +1,62 @@
+use std::{path::{Path, PathBuf}, collections::HashSet, fs::{self, File}, io::{Read, self}};
+
 #[cfg(test)]
 mod tests {
+    use std::{thread, time::Duration, path::Path, fs};
 
+    use rust_git::{servers::server::Server, handlers::clone::handler_clone, vcs::version_control_system::VersionControlSystem};
+
+    use crate::vcs::clone::compare_directories;
 
     #[test]
     pub fn test_01_run_server_for_command_clone()-> Result<(), std::io::Error>{ 
-
-        
-
+        let client_path = Path::new("tests/clone");
+        let server_path = Path::new("tests/test_folder");
+        let _ = thread::spawn( || {
+            let _ = Server::server("tests/test_folder".to_string());
+        });
+        let _ = VersionControlSystem::init(client_path, Vec::new());
+        let _ = handler_clone("git clone tests/clone".to_string());
+        assert!(
+            compare_directories(&client_path.join(".rust_git").join("objects"), &server_path.join("tests").join("clone").join(".rust_git").join("objects"))?,
+            "Los directorios no son iguales"
+        );
+        fs::remove_dir_all(client_path)?;
         Ok(())
     }
+}
 
+fn compare_files(file1: &Path, file2: &Path) -> io::Result<bool> {
+    let mut buf1 = Vec::new();
+    let mut buf2 = Vec::new();
 
+    File::open(file1)?.read_to_end(&mut buf1)?;
+    File::open(file2)?.read_to_end(&mut buf2)?;
+
+    Ok(buf1 == buf2)
+}
+
+fn compare_directories(dir1: &Path, dir2: &Path) -> io::Result<bool> {
+    let entries1: Vec<PathBuf> = fs::read_dir(dir1)?.map(|entry| entry.unwrap().path()).collect();
+    let entries2: Vec<PathBuf> = fs::read_dir(dir2)?.map(|entry| entry.unwrap().path()).collect();
+
+    if entries1.len() != entries2.len() {
+        return Ok(false);
+    }
+
+    for entry1 in entries1 {
+        let entry2 = dir2.join(entry1.file_name().unwrap());
+
+        if entry1.is_dir() {
+            if !compare_directories(&entry1, &entry2)? {
+                return Ok(false);
+            }
+        } else {
+            if !compare_files(&entry1, &entry2)? {
+                return Ok(false);
+            }
+        }
+    }
+
+    Ok(true)
 }
