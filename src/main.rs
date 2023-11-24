a47751781fce07e0f9a6682f2fbba1cd64595bf7
gmovia <gmovia@fi.uba.ar>
2023-11-24 12:41:41 -0300

    add interface

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ fn main() -> Result<(), std::io::Error>{
     // VersionControlSystem::init(Path::new("test1"), Vec::new());
     // //let _ = VersionControlSystem::init(Path::new("clone"), Vec::new());
 
-    loop{
-        let mut input = String::new();
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut input).unwrap();
-        handler_command( &input);
-    }
-    // let interface = RustInterface::new();
-    // let _ = interface.impl_interface();
+    //loop{
+    //    let mut input = String::new();
+    //    io::stdout().flush().unwrap();
+    //    io::stdin().read_line(&mut input).unwrap();
+    //    handler_command( &input);
+    //}
+    let interface = RustInterface::new();
+    let _ = interface.impl_interface();
+    Ok(())
 }
