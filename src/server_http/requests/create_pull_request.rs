fb024d737d009f3d767dd113ce8e4e35113129a1
ldiazcto <ldiazc@fi.uba.ar>
2023-12-15 11:17:33 -0300

    add application/xml as other type in body

--- src/server_http/requests/create_pull_request.rs
+++ src/server_http/requests/create_pull_request.rs
@@ -1,12 +1,15 @@
-
 use std::net::TcpStream;
 
 use serde::{Serialize, Deserialize};
+use serde_xml_rs::from_str as xml_from_str;
 
-use crate::{server_http::{sender::{send_server_error_msg, send_response, send_error}}, pull_request::{controllers::pull_request::PullRequest}};
+use crate::{
+    server_http::sender::{send_server_error_msg, send_response, send_error},
+    pull_request::controllers::pull_request::PullRequest,
+};
 
 #[derive(Serialize, Deserialize)]
-pub struct JsonCreatePR{
+pub struct StructCreatePR {
     title: Option<String>,
     body: Option<String>,
     head_repo: Option<String>,
@@ -15,7 +18,7 @@ pub struct JsonCreatePR{
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct CreatePullRequest{
+pub struct CreatePullRequest {
     pub title: Option<String>,
     pub body: Option<String>,
     pub head_repo: String,
@@ -28,15 +31,39 @@ pub struct CreatePullRequest{
 
 #[derive(Serialize, Deserialize)]
 pub struct ResponseOkCreatePullRequest {
-    id: String
+    id: String,
 }
 
 impl CreatePullRequest {
+    pub fn response_create_pull_request_object(
+        json_body: &str,
+        path: String,
+        stream: &mut TcpStream,
+        pull_request: PullRequest,
+        media_type: &str,
+    ) -> Result<CreatePullRequest, std::io::Error> {
+        match media_type {
+            "application/json" => Self::deserialize_json(json_body, path, stream, pull_request),
+            "application/xml" => Self::deserialize_xml(json_body, path, stream, pull_request),
+            _ => {
+                println!("Tipo de media no soportado: {}", media_type);
+                send_server_error_msg(stream);
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Tipo de media no soportado",
+                ))
+            }
+        }
+    }
 
-    pub fn response_create_pull_request_object(json_body: &str, path: String, stream: &mut TcpStream, pull_request: PullRequest) -> Result<CreatePullRequest, std::io::Error> {
-        println!("JSON Body: {}", json_body);
-        if let Ok(request) = serde_json::from_str::<JsonCreatePR>(json_body) {            
-            let head_vec: Vec<&str> = request.head.split(":").collect();
+    fn deserialize_json(
+        json_body: &str,
+        path: String,
+        stream: &mut TcpStream,
+        pull_request: PullRequest,
+    ) -> Result<CreatePullRequest, std::io::Error> {
+        if let Ok(request) = serde_json::from_str::<StructCreatePR>(json_body) {
+            let head_vec: Vec<&str> = request.head.split(':').collect();
 
             let mut create = CreatePullRequest {
                 title: request.title,
@@ -54,16 +81,59 @@ impl CreatePullRequest {
             }
 
             match pull_request.create(&mut create) {
-                Ok( response ) => { send_response(stream, ResponseOkCreatePullRequest { id: response})},
-                Err( error_code ) => { send_error(stream, error_code.to_string()) }
-            }; 
-            
-            return Ok(create)
+                Ok(response) => send_response(stream, ResponseOkCreatePullRequest { id: response }),
+                Err(error_code) => send_error(stream, error_code.to_string()),
+            };
+
+            Ok(create)
         } else {
             println!("Error al deserializar el mensaje: trailing characters");
             send_server_error_msg(stream);
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Error parsing request"))
-        };
+            Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Error parsing request",
+            ))
+        }
     }
 
+    fn deserialize_xml(
+        xml_body: &str,
+        path: String,
+        stream: &mut TcpStream,
+        pull_request: PullRequest,
+    ) -> Result<CreatePullRequest, std::io::Error> {
+        if let Ok(request) = xml_from_str::<StructCreatePR>(xml_body) {
+            println!("XMLLL");
+            let head_vec: Vec<&str> = request.head.split(':').collect();
+
+            let mut create = CreatePullRequest {
+                title: request.title,
+                body: request.body,
+                head_repo: "amoralejo/algo1".to_string(),
+                base_repo: path,
+                head: head_vec[1].to_owned(),
+                base: request.base,
+                username: head_vec[0].to_owned(),
+                mergeable: false,
+            };
+
+            if request.head_repo.is_none() {
+                create.head_repo = create.base_repo.clone()
+            }
+
+            match pull_request.create(&mut create) {
+                Ok(response) => send_response(stream, ResponseOkCreatePullRequest { id: response }),
+                Err(error_code) => send_error(stream, error_code.to_string()),
+            };
+
+            Ok(create)
+        } else {
+            println!("Error al deserializar el mensaje: trailing characters");
+            send_server_error_msg(stream);
+            Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Error parsing request",
+            ))
+        }
+    }
 }
--- src/server_http/web_server.rs
+++ src/server_http/web_server.rs
@@ -72,7 +72,9 @@ impl WebServer {
             Ok(bytes_read) => {
                 let request_str = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
                 println!("Received request: {}", request_str);
-    
+
+
+                
                 if let Some(header_end) = request_str.find("\r\n\r\n") {
                     let _ = Self::parse_request(header_end, &request_str, stream, server_path);
                 } else {
@@ -90,13 +92,16 @@ impl WebServer {
         let pull_request = PullRequest::init(&server_path);
         let json_header = &request_str[..header_end]; 
         let json_body = &request_str[header_end + 4..];
-    
+        println!("JSON HEADER ---> {} ", json_header);
+
+        let media_type = Self::get_content_type(json_header);
         let received_request = Self::get_received_request(json_header)?;
         let received_vec: Vec<&str> = received_request.split_whitespace().collect();
         let path: Vec<&str> = received_vec[1].split("/").collect();
         println!("PATH LEN: {}",path.len());
+
         match (received_vec[0], path.len() - 1) {
-            ("POST", 4) => {let _ = CreatePullRequest::response_create_pull_request_object(json_body, format!("{}/{}",path[2],path[3]),stream,pull_request);},
+            ("POST", 4) => {let _ = CreatePullRequest::response_create_pull_request_object(json_body, format!("{}/{}",path[2],path[3]),stream,pull_request, &media_type);},
             ("GET", 4) => {let _ = ListPullRequests::response_list_pull_request_object(json_body, stream, format!("{}/{}",path[2],path[3]),pull_request);},
             ("GET", 5) => {let _ = GetPullRequest::get_pull_request(stream, pull_request, format!("{}/{}",path[2],path[3]), path[5].to_owned());},
             ("GET", 6) => {let _ = ListCommitsPullRequest::list_commits_pull_request(stream, pull_request, format!("{}/{}",path[2],path[3]), path[5].to_string());},
@@ -108,6 +113,18 @@ impl WebServer {
         println!("            -----> {}", received_request);
         println!("PATH: {:?}", path);
         Ok(())
+    }
+
+    fn get_content_type(json_header: &str) -> String {
+        let lineas: Vec<&str> = json_header.lines().collect();
+    
+        for linea in lineas {
+            if linea.contains("Content-Type") {
+                let partes: Vec<&str> = linea.split(':').collect();
+                return partes[1].trim().to_string();
+            }
+        }
+        "None".to_string()
     }
 
     fn get_received_request(header: &str) -> Result<String, std::io::Error> {
